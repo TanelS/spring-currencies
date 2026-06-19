26370c75df5d876ac75ab4c3b272b949ab13bdf5
Tanel Sitska <TanelS@users.noreply.github.com>
2026-06-19 20:48:24 +0300

    Add `/currency-testing/rates` endpoint to TestController for fetching exchange rates based on base currency and symbols

--- src/main/java/org/home/currencies/controller/TestController.java
+++ src/main/java/org/home/currencies/controller/TestController.java
@@ -1,10 +1,14 @@
 package org.home.currencies.controller;
 
 import org.home.currencies.dto.CurrenciesApiResponse;
+import org.home.currencies.dto.RatesApiResponse;
 import org.home.currencies.service.ApiService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -20,4 +24,11 @@ public class TestController {
     public CurrenciesApiResponse getCurrencies() {
         return service.getCurrencies();
     }
+
+    @GetMapping("/rates")
+    public RatesApiResponse getRates(@RequestParam String baseCurrency, @RequestParam List<String> symbols) {
+        return service.getRates(baseCurrency, symbols);
+    }
+
+
 }
