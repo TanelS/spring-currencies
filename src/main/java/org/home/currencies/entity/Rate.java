abd746f58863011634c77a391c79e326a84f57eb
Tanel Sitska <TanelS@users.noreply.github.com>
2026-06-20 20:36:17 +0300

    Add `Rate` entity and update `Currency` entity with relationships and Lombok annotations; configure Hibernate DDL auto-update

---
+++ src/main/java/org/home/currencies/entity/Rate.java
@@ -0,0 +1,38 @@
+package org.home.currencies.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Entity
+@Table(name = "rate", indexes = {
+        @Index(name = "idx_rate_rate", columnList = "rate"),
+        @Index(name = "idx_rate_rate_date", columnList = "rate_date"),
+        @Index(name = "idx_rate_createdate", columnList = "createDate"),
+        @Index(name = "idx_rate_modifieddate", columnList = "modifiedDate")
+}, uniqueConstraints = {
+        @UniqueConstraint(name = "uc_rate_currency_id", columnNames = {"currency_id", "base_currency_id", "rate_date"})
+})
+@Getter
+@Setter
+public class Rate extends CommonColumns {
+
+    @Column(nullable = false,  precision = 20, scale = 10, comment = "Currency rate agains base currency")
+    private BigDecimal rate;
+
+    @Column(nullable = false, comment = "Currency rate date agains base currency")
+    private Instant rate_date;
+
+    @ManyToOne
+    @JoinColumn(name = "currency_id", comment = "Currency ID", nullable = false)
+    private Currency currency;
+
+    @ManyToOne
+    @JoinColumn(name = "base_currency_id", comment = "Base currency ID", nullable = false)
+    private Currency base_currency;
+
+
+}
