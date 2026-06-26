c7e0e05a2497be01b5666a0da7d384aacd4cbede
Tanel Sitska <TanelS@users.noreply.github.com>
2026-06-26 11:51:21 +0300

    Add TODO comment in `CurrencyResponse` for future improvements

--- src/main/java/org/home/currencies/dto/CurrencyResponse.java
+++ src/main/java/org/home/currencies/dto/CurrencyResponse.java
@@ -2,6 +2,7 @@ package org.home.currencies.dto;
 import org.home.currencies.entity.Currency;
 import java.util.List;
 
+// TODO this has to be redone!
 public record CurrencyResponse(
         Currency base,
         List<Currency> rates
